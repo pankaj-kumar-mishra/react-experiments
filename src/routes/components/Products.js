d4c8a4f906027c1345492a68b492154811e55c34
Pankaj Mishra <pankaj_mishra@epam.com>
2023-07-25 22:42:24 +0530

    dynamic params handled

--- src/routes/components/Products.js
+++ src/routes/components/Products.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import styles from "../appRoutes.module.css";
 
 const Products = () => {
+  const [data, setData] = useState([
+    { id: 1, name: "Product One", price: 100 },
+    { id: 2, name: "Product Two", price: 200 },
+    { id: 3, name: "Product Three", price: 300 },
+    { id: 4, name: "Product Four", price: 400 },
+    { id: 5, name: "Product Five", price: 500 },
+  ]);
+
   return (
     <div>
       <h1>Products</h1>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link className={styles.link} to="/products/1">
-            Apple
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link className={styles.link} to="/products/2">
-            Orange
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link className={styles.link} to="/products/3">
-            Graph
-          </Link>
-        </li>
+        {data.map((item) => (
+          <li key={item.id} className={styles.li}>
+            <Link
+              className={styles.link}
+              to={`/products/${item.id}`}
+              state={item}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
       </ul>
-      <Outlet />
+
+      <Outlet context={data} />
     </div>
   );
 };
